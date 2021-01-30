3ce6688d4cd4e0cc0d786a604cca23f833cc38cf
Volodymyr Boichentsov <sakristx@gmail.com>
2021-01-30 10:58:27 +0000

    update to latest packages

--- Package.swift
+++ Package.swift
@@ -6,15 +6,15 @@ let package = Package(
     name: "OpenGLExampleApp",
     dependencies: [
         .package(url: "https://github.com/sakrist/SwiftMath", from: "3.2.3"),
-        .package(url: "https://github.com/sakrist/GLAppBase", from: "0.0.1")
+        .package(url: "https://github.com/sakrist/GLAppBase", from: "0.0.2")
     ],
     targets: [
         .target( name: "app", dependencies: [ "SwiftMath", "GLAppBase" ]),
     ]    
 )
 
-
-//#if os(Android)
+ 
+#if os(Android) 
 package.products.append(.library(name: "OpenGLExampleApp", type: .dynamic, targets: ["app"]))
 package.dependencies.append(.package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .exact("1.13.0")))
 package.dependencies.append(.package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"))
@@ -26,10 +26,11 @@ package.targets += [
     .systemLibrary(name: "CAndroidLog"),
     .systemLibrary(name: "CAndroidGL")
 ]
-//#endif
+#endif
 
 #if os(Linux)
-package.dependencies.append(.package(url: "https://github.com/sakrist/COpenGL.swift.git", from:"1.0.6"))
-package.dependencies.append(.package(url: "https://github.com/sakrist/CX11.swift.git", from:"1.0.4"))
+package.dependencies.append(.package(name: "COpenGL", url: "https://github.com/sakrist/COpenGL.swift.git", from:"1.0.7"))
+package.dependencies.append(.package(name: "CX11", url: "https://github.com/sakrist/CX11.swift.git", from:"1.0.5"))
+package.targets[0].dependencies += ["COpenGL", "CX11"]
 #endif
 
