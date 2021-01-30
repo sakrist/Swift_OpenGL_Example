faf705c3c3c79a08a779a9c0f110dbc434ef03fc
Volodymyr Boichentsov <sakristx@gmail.com>
2021-01-30 10:10:07 +0000

    update versions

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ package.targets += [
 //#endif
 
 #if os(Linux)
-package.dependencies.append(.package(url: "https://github.com/sakrist/COpenGL.swift.git", from:"1.0.5"))
-package.dependencies.append(.package(url: "https://github.com/sakrist/CX11.swift.git", from:"1.0.3"))
+package.dependencies.append(.package(url: "https://github.com/sakrist/COpenGL.swift.git", from:"1.0.6"))
+package.dependencies.append(.package(url: "https://github.com/sakrist/CX11.swift.git", from:"1.0.4"))
 #endif
 
